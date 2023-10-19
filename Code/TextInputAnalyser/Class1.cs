d7ce3ecfa42561b5a90f5b2d9e10766b5bef84a3
warrenmagagula <magagulaw@out.co.za>
2023-10-19 14:37:26 +0200

    Project initialisation and structure

---
+++ Code/TextInputAnalyser/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace TextInputAnalyser;
+
+public class Class1
+{
+}
---
+++ Tests/TextInputAnalyser.Tests/UnitTest1.cs
@@ -0,0 +1,9 @@
+namespace TextInputAnalyser.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+    }
+}
